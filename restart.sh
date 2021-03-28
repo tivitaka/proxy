8dc47005d35376c3fe817616c114a883da20e76f
tivitaka <hphauct@gmail.com>
2021-03-28 15:23:58 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -22,7 +22,6 @@ setgid 65535
 setuid 65535
 flush
 
-users $(awk -F "/" 'BEGIN{ORS="";} {print $1 ":CL:" $2 " "}' ${WORKDATA})
 $(awk -F "/" '{print "auth iponly\n" \
 "allow * 115.74.50.50  \n" \
 "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
