93964eedf096ba7c8829994a5801748698e70060
tivitaka <hphauct@gmail.com>
2021-03-28 14:20:10 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ upload_proxy() {
 
 }
 gen_data() {
-    seq $FIRST_PORT $LAST_PORT | while read port; do
+    seq 10000 10100 | while read port; do
         echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
     done
 }
@@ -90,14 +90,6 @@ mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
-
-echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
-
-echo "How many proxy do you want to create? Example 500"
-read COUNT
-
-FIRST_PORT=10000
-LAST_PORT=$(($FIRST_PORT + $COUNT))
 
 gen_data >$WORKDIR/data.txt
 gen_iptables >$WORKDIR/boot_iptables.sh
