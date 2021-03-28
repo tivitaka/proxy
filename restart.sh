e13deaeed8b0f60f30f930c52803496f113ca7ab
tivitaka <hphauct@gmail.com>
2021-03-28 15:15:02 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -54,6 +54,7 @@ cd $WORKDIR
 rm -rf $WORKDIR/data.txt
 rm -rf $WORKDIR/boot_iptables.sh
 rm -rf $WORKDIR/boot_ifconfig.sh
+rm -rf /usr/local/etc/3proxy/3proxy.cfg
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
