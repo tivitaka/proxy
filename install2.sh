bab6ee508bc9be3f94dd05e345ab789ba751e689
tivitaka <hphauct@gmail.com>
2021-04-07 14:53:11 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -93,7 +93,6 @@ WORKDATA="${WORKDIR}/data.txt"
 mkdir $WORKDIR && cd $_
 
 IP4=$(curl -4 -s icanhazip.com)
-setIpv6Psychz
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
 
 echo "Internal ip = ${IP4}. Exteranl sub for ip6 = ${IP6}"
