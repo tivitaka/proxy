4220b441b6c56e786b20a855d38244ba16d36162
tivitaka <hphauct@gmail.com>
2021-04-08 22:43:16 +0700

    Update install2.sh

--- install2.sh
+++ install2.sh
@@ -68,7 +68,8 @@ EOF
 echo "working folder = /home/proxy-installer"
 WORKDIR="/home/proxy-installer"
 WORKDATA="${WORKDIR}/data.txt"
-cd '/home/proxy-installer'
+
+cd $WORKDIR
 
 IP4=$(curl -4 -s icanhazip.com)
 IP6=$(curl -6 -s icanhazip.com | cut -f1-4 -d':')
